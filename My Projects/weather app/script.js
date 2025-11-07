59d99992603f998e8432efda86f1197df0e289b0
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2025-11-07 15:50:45 +0530

    Added forcast

--- My Projects/weather app/script.js
+++ My Projects/weather app/script.js
@@ -29,8 +29,52 @@ async function getWeather() {
         <p>${data.weather[0].description}</p>
         <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png">
         `;
+        GetForcast(cityName);
     }
     catch {
         error.textContent = "Network error"
     }
+}
+async function GetForcast(cityName) {
+    let apiKey = "b67e03e46b2b46786ca6807328c24da0";
+    let forcastDiv = document.getElementById("forcast");
+    forcastDiv.innerHTML = "";
+
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
+    let respose = await fetch(url);
+    if (!respose.ok) return;
+
+    let data = await respose.json();
+
+    let dailyData = {};
+    data.list.forEach(item => {
+        let date = item.dt_txt.split(" ")[0];
+        if (!dailyData[date]) {
+            dailyData[date] = {
+                temps: [],
+                icon: item.weather[0].icon,
+                desc: item.weather[0].description
+            };
+        }
+        dailyData[date].temps.push(item.main.temp);
+    });
+
+    let days = Object.keys(dailyData).slice(0, 5);
+    days.forEach(date => {
+        let temps = dailyData[date].temps;
+        let max = Math.max(...temps).toFixed(1)
+        let min = Math.min(...temps).toFixed(1)
+        let icon = dailyData[date].icon;
+        let desc = dailyData[date].desc;
+
+        console.log(date);
+        forcastDiv.innerHTML += `
+                <div class="forcast-day">
+                    <p>${new Date(date).toLocaleDateString("en-US", { weekday: "short" })}</p>
+                    <img src="http://openweathermap.org/img/wn/${icon}.png" alt="${desc}">
+                    <p>${desc}</p>
+                    <p>${min}° / ${max}°C</p>
+                </div>
+                `;
+    });
 }
