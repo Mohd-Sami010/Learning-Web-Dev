21c7d1a4a781ad3ba8bb8a04469dacdef69e0de6
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2025-12-11 12:25:29 +0530

    Learning JS

---
+++ Learning JS/01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "sami@gmail.com"
+var accountPasswd = "321567"
+accountCity = "Jaipur"
+
+// accountId = 7867
+accountEmail = "abc@abc.com"
+accountPasswd = "49238"
+accountCity = "Delhi"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPasswd, accountCity])
---
+++ Learning JS/01_Basics/02_conversionOperation.js
@@ -0,0 +1,11 @@
+let greet = "Hello";
+let name = "Sami";
+
+let str = greet + " " + name;
+console.log(str);
+console.log("1" + 2);
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+console.log(+true);
+console.log(+"");
