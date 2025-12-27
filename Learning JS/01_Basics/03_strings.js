fe9004c76f090a62cdd8e4be4fbac915c2b947b0
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2025-12-27 22:20:22 +0530

    Strings in JS

---
+++ Learning JS/01_Basics/03_strings.js
@@ -0,0 +1,14 @@
+const name = new String("Mohd Sami")
+const repoCount = 16;
+
+console.log("My name is " + name + " and I have " + repoCount + " repos on github.")
+console.log(`My name is ${name} and I have ${repoCount} repos on github.`)
+
+console.log(name[0])
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.charAt(2))
+console.log(name.indexOf('a'))
+
+console.log(name.substring(0,4))
+console.log(name.slice(-8, 4))
