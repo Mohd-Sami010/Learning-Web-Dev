5499db1df308e5b7aa12298e1e56bec84bc87028
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2026-01-03 23:53:22 +0530

    Date and Time in js

---
+++ Learning JS/01_Basics/05_DateAndTime.js
@@ -0,0 +1,10 @@
+// let myDate =  new Date()
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+// let myCreatedDate = new Date(2024, 0, 23)
+// let myCreatedDate = new Date(2024, 0, 23, 6, 57)
+let myCreatedDate = new Date("2025-02-23")
+console.log(myCreatedDate.toLocaleString())
